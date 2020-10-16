87430bc45f1da628b9c1066fb33931de3514d0b4
sheldonej <56203719+sheldonej@users.noreply.github.com>
2020-10-16 17:27:07 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,17 +2,12 @@ from __future__ import print_function
 from flask import Flask, render_template
 from os.path import exists
 from os import makedirs
-from os.path import join, dirname
 from dotenv import load_dotenv
 from datetime import datetime
 from psycopg2 import connect
 import psycopg2
 import json
 import os
-
-
-dotenv_path = join(dirname(__file__), '.env')
-load_dotenv(dotenv_path)
 
 connection = psycopg2.connect(  user = DB_USER,
                                 password = DB_PASS,
