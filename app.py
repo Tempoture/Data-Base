efb17d464778b24782fdddea75388293bed79c5a
Sheldon Jackson <Sheldon.ej@gmail.com>
2020-10-31 15:30:22 -0400

    Update app.py
    adding request ass it is used later

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import create_engine
 from datetime import datetime
